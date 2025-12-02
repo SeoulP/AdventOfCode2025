b061060b08731661647ecf8c6326335564859196
SeoulP <seoul.peterson@outlook.com>
2025-12-01 18:43:14 -0600

    Add files via upload

---
+++ Part2.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using AdventOfCode2025.Day_1;
+
+class Day1Part2
+{
+    
+    public static void Run()
+    {
+        Console.WriteLine("Starting Day 1-2");
+
+        var rotations = GetInput();
+        
+        Locks lockObject = new Locks(50);
+        foreach (var rotation in rotations)
+        {
+            lockObject.RotateAndCountAllZeroes(rotation);
+        }
+
+        Console.WriteLine(lockObject.GetNumberOfZeroes);
+    }
+    
+    private static List<Rotations> GetInput()
+    {
+        List<Rotations> rotations = new List<Rotations>();
+        var input = File.ReadAllLines($@"Inputs\input_day1.txt").ToList();
+
+        foreach (String line in input)
+        {
+            Rotations rotation = new Rotations();
+            rotation.RotationDirection = rotation.GetDirection(line.Substring(0, 1));
+            rotation.Degrees = int.Parse(line.Substring(1));
+            
+            rotations.Add(rotation);
+        }
+        
+        return rotations;
+    }   
+}
