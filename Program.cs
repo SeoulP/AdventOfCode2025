4605ad27e608f3071a139a162d4f8c7ef7242e98
SeoulP <seoul.peterson@outlook.com>
2025-12-01 18:48:04 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Starting Day 1");
+Console.WriteLine("Select a Day and Part (ex: 1.1)");
+bool isStillProcessing = true;
 
-Day1Part1.Run();
+do
+{
+    Console.Write("Enter Day: ");
+    var input = Console.ReadLine();
+    switch (input)
+    {
+        case "1.1":
+            Day1Part1.Run();
+            isStillProcessing = false;
+            break;
+        case "1.2":
+            Day1Part2.Run();
+            isStillProcessing = false;
+            break;
+        default:
+            Console.WriteLine("This is not a valid day.");
+            isStillProcessing = true;
+            break;
+    }
+} while (isStillProcessing);
+
+Console.WriteLine("Day finished.");
